d1d2337436f3df005c9d86f1d3218edc2e90fde3
Guilherme Augusto <guilherme_amsilva@hotmail.com>
2018-08-07 14:10:42 -0300

    Connecting verbs with database

--- RestASPNETUdemy/RestASPNETUdemy/Startup.cs
+++ RestASPNETUdemy/RestASPNETUdemy/Startup.cs
@@ -4,10 +4,12 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using RestASPNETUdemy.Model.Context;
 using RestASPNETUdemy.Services;
 using RestASPNETUdemy.Services.Implementation;
 
@@ -25,6 +27,9 @@ namespace RestASPNETUdemy
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+
+            var connection = Configuration["MySqlConnection:MySqlConnectionString"];
+            services.AddDbContext<MySQLContext>(options => options.UseMySql(connection));
             services.AddMvc();
 
             //Dependency Injection
